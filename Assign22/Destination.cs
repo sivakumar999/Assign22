e14c8cf4fa62f85499295b782c6824b2acfff964
sivakumar999 <sivakumar59498@gmail.com>
2023-08-03 15:31:00 +0530

    Done

---
+++ Assign22/Destination.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign22
+{
+    public class Destination
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public string Department { get; set; }
+    }
+}
---
+++ Assign22/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Assign22
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            Source source = new Source
+            {
+                Id = 11,
+                Name = "siva",
+                Age = 30
+
+
+            };
+            /*Console.WriteLine("Enter a id :");
+            source.Id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a name :");
+            source.Name = Console.ReadLine();
+            Console.WriteLine("Enter a age :");
+            source.Age = int.Parse(Console.ReadLine());*/
+
+            Destination destination = new Destination();
+
+            PropertyMaping.MapProperties(source, destination);
+
+            // Display the values in the Source class
+            Console.WriteLine("Source Class Properties:");
+            Console.WriteLine($"Id: {source.Id}");
+            Console.WriteLine($"Name: {source.Name}");
+            Console.WriteLine($"Age: {source.Age}");
+
+            Console.WriteLine();
+
+            // Display the values in the Destination class
+            Console.WriteLine("Destination Class Properties:");
+            Console.WriteLine($"Id: {destination.Id}");
+            Console.WriteLine($"Name: {destination.Name}");
+            Console.WriteLine($"Salary: {destination.Salary}");
+            Console.WriteLine($"Department: {destination.Department}");
+
+            Console.ReadKey();
+        }
+    }
+}
